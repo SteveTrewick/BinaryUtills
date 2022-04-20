d6512bb2e0809c213213b52e4f17696726bf38f8
Steve Trewick <steve.trewick@icloud.com>
2022-04-20 19:40:31 +0100

    hex

--- Sources/BinaryUtills/Hex.swift
+++ Sources/BinaryUtills/Hex.swift
@@ -1,8 +1,16 @@
-//
-//  File.swift
-//  
-//
-//  Created by Steve Trewick on 20/04/2022.
-//
 
 import Foundation
+
+extension FixedWidthInteger {
+  
+  public func toHex() -> String {
+    
+    var hexes : [String] = []
+    
+    for shift in stride(from: 0, to: self.bitWidth - 4, by: 4) {
+      let hex = UInt8((self >> shift) & 0xff)
+      hexes.append( String(format: "%02X", hex) )
+    }
+    return hexes.reversed().joined(separator: "")
+  }
+}
