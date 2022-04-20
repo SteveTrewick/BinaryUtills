5db327eaa3e12fc61e6ed834f9a98cdc1eb2d9c1
Steve Trewick <steve.trewick@icloud.com>
2022-04-20 19:58:15 +0100

    gardening

--- Sources/BinaryUtills/Hex.swift
+++ Sources/BinaryUtills/Hex.swift
@@ -3,14 +3,19 @@ import Foundation
 
 extension FixedWidthInteger {
   
+  // I'm sure there is a nicer way to do this,
+  // me, I like quick and dirty.
+  
   public func toHex() -> String {
     
-    var hexes : [String] = []
+      var hexes : [String] = []
     
-    for shift in stride(from: 0, to: self.bitWidth, by: 4) {
-      let hex = UInt8((self >> shift) & 0xf)
-      hexes.append( String(format: "%X", hex) )
-    }
-    return hexes.reversed().joined(separator: "")
+      for shift in stride(from: 0, to: self.bitWidth, by: 4) {
+      
+          let hex = UInt8((self >> shift) & 0xf)
+      
+          hexes.append( String(format: "%X", hex) )
+      }
+      return hexes.reversed().joined(separator: "")
   }
 }
