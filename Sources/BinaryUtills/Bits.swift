32e3b78f5a8ee455209ec6576c0f07c1be41d183
Steve Trewick <steve.trewick@icloud.com>
2022-04-20 20:36:02 +0100

    same deal for bits

--- Sources/BinaryUtills/Bits.swift
+++ Sources/BinaryUtills/Bits.swift
@@ -1,2 +1,23 @@
 
 import Foundation
+
+
+extension FixedWidthInteger {
+  
+  
+  public func toBits() -> [Int] {
+    
+    Array (
+      (0..<self.bitWidth).map { Int((self >> $0) & 1) }
+                         .reversed()
+    )
+  }
+  
+  
+  public func toBitString() -> String {
+    
+    self.toBits().map { String($0) }
+                 .joined(separator: "")
+  }
+  
+}
--- Tests/BinaryUtillsTests/BinaryUtillsTests.swift
+++ Tests/BinaryUtillsTests/BinaryUtillsTests.swift
@@ -15,4 +15,21 @@ final class BinaryUtillsTests: XCTestCase {
     XCTAssert( u32.toHex() == "DEADBEEF")
     XCTAssert( u64.toHex() == "DEADBEEFCAFEF00D")
   }
+  
+  
+  func testBits() {
+    
+    let u16 : UInt16 = 0b1111_0000_1111_0011
+    
+    XCTAssert(u16.toBits() == [1,1,1,1,0,0,0,0,1,1,1,1,0,0,1,1] )
+  
+  }
+  
+  func testBitString() {
+    
+    let u16 : UInt16 = 0b1111_0000_1111_0011
+    
+    XCTAssert( u16.toBitString() == "1111000011110011" )
+  }
+  
 }
