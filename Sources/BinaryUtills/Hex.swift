94db31dbee229e4c2636656b59c7415630bededb
Steve Trewick <steve.trewick@icloud.com>
2022-04-20 19:53:37 +0100

    hex test, also DUH!

--- Sources/BinaryUtills/Hex.swift
+++ Sources/BinaryUtills/Hex.swift
@@ -7,9 +7,9 @@ extension FixedWidthInteger {
     
     var hexes : [String] = []
     
-    for shift in stride(from: 0, to: self.bitWidth - 4, by: 4) {
-      let hex = UInt8((self >> shift) & 0xff)
-      hexes.append( String(format: "%02X", hex) )
+    for shift in stride(from: 0, to: self.bitWidth, by: 4) {
+      let hex = UInt8((self >> shift) & 0xf)
+      hexes.append( String(format: "%X", hex) )
     }
     return hexes.reversed().joined(separator: "")
   }
--- Tests/BinaryUtillsTests/BinaryUtillsTests.swift
+++ Tests/BinaryUtillsTests/BinaryUtillsTests.swift
@@ -2,5 +2,17 @@ import XCTest
 @testable import BinaryUtills
 
 final class BinaryUtillsTests: XCTestCase {
+  
+  func testHexExtension() {
     
+    let u8  : UInt8  = 0xef
+    let u16 : UInt16 = 0xbeef
+    let u32 : UInt32 = 0xdeadbeef
+    let u64 : UInt64 = 0xdeadbeefcafef00d
+    
+    XCTAssert( u8.toHex()  == "EF")
+    XCTAssert( u16.toHex() == "BEEF")
+    XCTAssert( u32.toHex() == "DEADBEEF")
+    XCTAssert( u64.toHex() == "DEADBEEFCAFEF00D")
+  }
 }
